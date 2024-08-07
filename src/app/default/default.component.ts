4a81d7a282eb803b5434798bff82153ce0290542
Marco Montanari <sirmmo@gmail.com>
2024-08-07 19:21:56 +0200

    update

--- src/app/default/default.component.ts
+++ src/app/default/default.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 import { GravatarModule } from 'ngx-gravatar';
@@ -9,7 +10,7 @@ import { GravatarModule } from 'ngx-gravatar';
 @Component({
   selector: 'app-default',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, CommonModule, RouterModule, GravatarModule],
+  imports: [MatToolbarModule, MatButtonModule, CommonModule, RouterModule, GravatarModule, MatIconModule],
   templateUrl: './default.component.html',
   styleUrl: './default.component.scss'
 })
