c55fd1badbb5b7856eca38377c38ba0536dbec86
Marco Montanari <sirmmo@gmail.com>
2024-08-20 16:42:09 +0200

    update

--- src/app/party-master/qr/qr.component.ts
+++ src/app/party-master/qr/qr.component.ts
@@ -1,11 +1,11 @@
 import { Component, inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 import { QRCodeModule } from 'angularx-qrcode';
 
 @Component({
   selector: 'app-qr',
   standalone: true,
-  imports: [QRCodeModule],
+  imports: [QRCodeModule, MatDialogModule],
   templateUrl: './qr.component.html',
   styleUrl: './qr.component.scss'
 })
